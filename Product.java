3d7a92da5c0ee5677c52b7b270c303ef4c9eea61
Selcuk Buyuktanir <selcuk.buyuktanir@gmail.com>
2022-04-14 14:35:03 +0200

    selcuk_product: double Product.java

---
+++ Product.java
@@ -0,0 +1,20 @@
+public class Product
+{
+private String name = "Coca Cola";
+private int preis = 10;
+
+public Product (String name)
+{
+        setName(name);
+}
+
+public void setName(String name)
+{
+        this.name = "Coca Cola";
+}
+public String getName()
+{
+        return name;
+}
+}
+
