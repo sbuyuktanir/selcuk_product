e4219bfec7f4fe0418a98cc699e17ddb80a0cf8c
Selcuk Buyuktanir <selcuk.buyuktanir@gmail.com>
2022-04-14 14:11:40 +0200

    selcuk_product: create Product.java

---
+++ Product.java
@@ -0,0 +1,19 @@
+public class Product
+{
+private String name = "Coca Cola";
+
+public Product (String name)
+{
+        setName(name);
+}
+
+public void setName(String name)
+{
+        this.name = "Coca Cola";
+}
+public String getName()
+{
+        return name;
+}
+}
+
